5294b48b0a1be2e5d35732a3297accd2533842d3
孔祥俊 <kongxj@digiwin.com>
2024-11-26 16:17:39 +0800

    ops

--- stop.sh
+++ stop.sh
@@ -2,6 +2,7 @@
 
 # 容器配置
 CONTAINER_NAME="service-a"
+IMAGE_NAME="kkk2099/kkk:service-a-1.0"
 
 # 检查容器是否存在
 if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
@@ -25,3 +26,20 @@ if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
 else
    echo "Container $CONTAINER_NAME does not exist."
 fi
+
+# 检查并删除镜像
+if [ "$(docker images -q $IMAGE_NAME)" ]; then
+   echo "Removing image $IMAGE_NAME..."
+   docker rmi $IMAGE_NAME
+
+   if [ $? -eq 0 ]; then
+       echo "Image $IMAGE_NAME removed successfully."
+   else
+       echo "Failed to remove image $IMAGE_NAME."
+       exit 1
+   fi
+else
+   echo "Image $IMAGE_NAME does not exist."
+fi
+
+echo "Clean up completed successfully."
